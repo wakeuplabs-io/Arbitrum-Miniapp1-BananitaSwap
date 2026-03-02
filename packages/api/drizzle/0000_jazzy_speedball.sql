686dcd878414a5973357e1dbaf3e2b764c091866
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2026-03-02 12:22:50 -0300

    add db migrations

---
+++ packages/api/drizzle/0000_jazzy_speedball.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "auth_nonce" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"nonce" text NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL,
+	"used" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "auth_nonce_nonce_unique" UNIQUE("nonce")
+);
