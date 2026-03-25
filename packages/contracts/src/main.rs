7e3118aac97fcf1a8800fc24f50110a95a54ce62
juandahl <juandahl20@gmail.com>
2026-03-25 23:12:15 +1000

    feat: add rust contracts

--- packages/contracts/src/main.rs
+++ packages/contracts/src/main.rs
@@ -1,10 +1,22 @@
-#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
-
-#[cfg(not(any(test, feature = "export-abi")))]
-#[no_mangle]
-pub extern "C" fn main() {}
-
-#[cfg(feature = "export-abi")]
+// ABI export entrypoint. Run with `cargo run --features export-abi,<contract>`.
+//
+// Examples:
+//   cargo run --features export-abi,contract-router
+//   cargo run --no-default-features --features export-abi,contract-camelot
+//   cargo run --no-default-features --features export-abi,contract-uniswap-v3
 fn main() {
-	stylus_contracts::print_from_args();
+    #[cfg(all(feature = "export-abi", feature = "contract-router"))]
+    stylus_sdk::abi::export::print_from_args::<
+        arbitrum_miniapp_contracts_stylus::router::DexRouter,
+    >();
+
+    #[cfg(all(feature = "export-abi", feature = "contract-camelot"))]
+    stylus_sdk::abi::export::print_from_args::<
+        arbitrum_miniapp_contracts_stylus::adapters::camelot::CamelotAdapter,
+    >();
+
+    #[cfg(all(feature = "export-abi", feature = "contract-uniswap-v3"))]
+    stylus_sdk::abi::export::print_from_args::<
+        arbitrum_miniapp_contracts_stylus::adapters::uniswap_v3::UniswapV3Adapter,
+    >();
 }
